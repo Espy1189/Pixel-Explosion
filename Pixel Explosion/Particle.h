21da5cb366d5ecf5789c340902b312daed799418
Brandon Espinal <brandon.espinal14@gmail.com>
2018-08-12 03:49:57 -0500

    Add files via upload

---
+++ Pixel Explosion/Particle.h
@@ -0,0 +1,27 @@
+
+
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+namespace PixelExplosion {
+
+struct Particle {
+	double m_x;
+	double m_y;
+
+private:
+	double m_speed;
+	double m_direction;
+
+private:
+	void init();
+
+public:
+	Particle();
+	virtual ~Particle();
+	void update(int interval);
+};
+
+} /* namespace PixelExplosion */
+
+#endif /* PARTICLE_H_ */
